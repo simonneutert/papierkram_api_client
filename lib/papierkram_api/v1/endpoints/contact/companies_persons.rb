9c352256083209d10d97f4a742a1d1074c98df7f
Simon Neutert <simonneutert@users.noreply.github.com>
2023-06-28 20:01:30 +0200

    crud for persons (#45)

--- lib/papierkram_api/v1/endpoints/contact/companies_persons.rb
+++ lib/papierkram_api/v1/endpoints/contact/companies_persons.rb
@@ -21,6 +21,54 @@ module PapierkramApi
 
             get("#{@url_api_path}/contact/companies/#{company_id}/persons", query)
           end
+
+          def create( # rubocop:disable Metrics/ParameterLists
+            company_id:,
+            first_name:,
+            last_name:,
+            title: nil,
+            salutation: nil,
+            position: nil,
+            department: nil,
+            email: nil,
+            phone: nil,
+            mobile: nil,
+            fax: nil,
+            skype: nil,
+            comment: nil
+          )
+            body = {
+              first_name: first_name,
+              last_name: last_name,
+              title: title,
+              salutation: salutation,
+              position: position,
+              department: department,
+              email: email,
+              phone: phone,
+              mobile: mobile,
+              fax: fax,
+              skype: skype,
+              comment: comment
+            }
+
+            post("#{@url_api_path}/contact/companies/#{company_id}/persons", body)
+          end
+
+          def update_by(company_id:, id:, attributes: {})
+            raise ArgumentError, 'attributes must be a Hash' unless attributes.is_a?(Hash)
+            raise ArgumentError, 'company_id must be an Integer' unless company_id.is_a?(Integer)
+            raise ArgumentError, 'id must be an Integer' unless id.is_a?(Integer)
+
+            put("#{@url_api_path}/contact/companies/#{company_id}/persons/#{id}", attributes)
+          end
+
+          def delete_by(company_id:, id:)
+            raise ArgumentError, 'company_id must be an Integer' unless company_id.is_a?(Integer)
+            raise ArgumentError, 'id must be an Integer' unless id.is_a?(Integer)
+
+            delete("#{@url_api_path}/contact/companies/#{company_id}/persons/#{id}")
+          end
         end
       end
     end
